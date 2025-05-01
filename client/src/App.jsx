c309fac62aa3765149a5557fbbe8af0a103d3353
Neerajbagauli <bagaulineeraj1d@gmail.com>
2025-05-01 12:10:02 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredit from './pages/BuyCredit'
+import { Routes,Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+function App() {
+ 
+  return (
+    <div className='px-4 sm:px-10 md:px-14 lg:px-48 min-h-screen bg-gradient-to-b from-teal-50 to-slate-500'>
+      <Navbar/>
+      <Routes>
+        <Route path = '/' element = {<Home/>} />
+        <Route path = '/result' element = {<Result/>} />
+        <Route path = '/buy' element = {<BuyCredit/>} />
+      
+      
+      
+      </Routes>
+
+   
+    </div>
+  )
+}
+
+export default App
